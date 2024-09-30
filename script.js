ceea508d277c6bf6c0453a73c6a8e35f2f6a48f0
shivnandan-Gunagya <shivnandansoni@gunagya.in>
2024-09-30 17:59:40 +0530

    section 4 created

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.getElementById("hamburger")
 const MobilelistItems = document.querySelector(".MobilelistItems")
 const closenav = document.querySelector(".closenav")
+const cardGrid = document.getElementById("cardGrid")
 
 let isOpen = false
 hamburger.addEventListener("click", ()=>{
@@ -20,3 +21,26 @@ closenav.addEventListener("click", ()=>{
         MobilelistItems.classList.remove("openNav")
     }
 });
+
+
+
+for(let i =0; i<6; i++){
+    const card = document.createElement("div")
+    card.className = "card"
+
+    card.innerHTML = `
+        <img
+            src="https://i.ibb.co/7z2Hvqp/Rectangle-24.png"
+            alt="Rectangle-24"
+          />
+          <div class="cardtextArea">
+            <h4>LYNC Wallet SDK</h4>
+            <p>
+              Empowering developers to integrate account abstraction in their
+              game, dapp, metaverse and more in minutes.
+            </p>
+          </div>
+    `
+
+    cardGrid.appendChild(card)
+}
